42057e42964621a01ea298262257cd2ab7f655c3
Neil Middleton <neil@neilmiddleton.com>
2012-12-11 11:01:46 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CiTest::Application.config.secret_token = '08994f481ddca31ef3f969dda62bd0b5ab855262f28bee6aa5a78576ab5b95b14e31fee42a7c707b078544ad6d5ebaf3e5170bb01a4ce102387768902b3a98f7'
---
+++ test/unit/basic_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class BasicTest < ActiveSupport::TestCase
+
+  test 'assert the truth' do
+    true
+  end
+
+end
